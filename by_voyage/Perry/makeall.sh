feefdbb93c0d2c1b713ba3f2f16126f4e4554b4f
Philip Brohan <philip@brohan.org>
2015-05-21 15:07:11 +0100

    Add the original data and fix another couple of bad positions.

---
+++ by_voyage/Perry/makeall.sh
@@ -0,0 +1,13 @@
+# Make the IMMA file and diagnostic plots for Perry
+
+../../scripts/utilities/voyage_positions.perl --ship='Perry' > positions.out
+# Manually edited to make positions_Perry.xlsx & positions_Perry.csv
+# Further edited to make positions_Perry.csv.qc
+./qc_positions.perl < positions_Perry.csv.qc > positions_Perry.fmt
+../../scripts/utilities/voyage_t+p.perl --ship='Perry' > obs.out
+# Manually edited - fix dud dates to obs.qc.out
+./to_imma.perl > imma.out
+R --no-save < plot_voyage.R
+R --no-save < 20CR.extract.ship.R
+R --no-save < O_R_plot.R
+cp imma.out ../../imma/Perry.imma
