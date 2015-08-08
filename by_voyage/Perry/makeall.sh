fc417b600c922beb24a9415eee4b98419f7a4f8f
Philip Brohan <philip@brohan.org>
2015-08-08 20:03:46 +0100

    Updated stats to 2015-08-08.

--- by_voyage/Perry/makeall.sh
+++ by_voyage/Perry/makeall.sh
@@ -6,6 +6,7 @@
 ./qc_positions.perl < positions_Perry.csv.qc > positions_Perry.fmt
 ../../scripts/utilities/voyage_t+p.perl --ship='Perry' > obs.out
 # Manually edited - fix dud dates to obs.qc.out
+./qc_obs_dates.perl < obs.out.qc > obs.out.date.fmt
 ./to_imma.perl > imma.out
 R --no-save < plot_voyage.R
 R --no-save < 20CR.extract.ship.R
