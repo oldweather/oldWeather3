1714ade6eba50fc20893f7258409f04b043cdee7
Philip Brohan <philip@brohan.org>
2020-05-06 10:43:49 +0100

    Added scripts for unarchiving selected logbooks.

---
+++ scripts/logbooks_from_mass/make_unarchive.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+# Extract and unpack selected logbook images from MASS
+
+import csv
+import re
+import warnings
+import os
+
+# Get the Moose directory listings
+with open('ls_images','r') as f:
+    archived = [line.rstrip() for line in f.readlines()]
+
+moo_f = open('to_retrieve.txt','w')
+miss_f = open('missing.txt','w')
+
+# Get the list of logbooks Clive has selected
+with open('SA_logs_Inventory.csv','r') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    next(csv_reader) # discard the headers
+    for row in csv_reader:
+        ref = row[12]
+        ref=ref.strip()
+        if len(ref)==0 : continue
+        # convert to a regexp - names don't always match exactly
+        ref=ref.replace('/','_')
+        ref=ref.replace('_','[/\s\._\-]')
+        ref=ref.replace(' ','[/\s\._\-]')
+        ref=ref.replace('&','+')
+        ref=ref.replace('+','\+')
+        # Find the archived file that matches
+        r = re.compile(ref+".*\.tgz")
+        matched=False
+        for line in archived:
+            if r.search(os.path.basename(line)) is not None:
+                print(ref+" - "+line)
+                moo_f.write("%s\n" % line)
+                matched=True
+                continue
+        if matched == False:
+            print("Failed "+ref)
+            miss_f.write("%s\n" % row[12])
+
+moo_f.close()
+miss_f.close()
+        
+        
---
+++ scripts/logbooks_from_mass/run_unarchive.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+# Retrieve the selected list of logs from MASS
+
+import os
+import os.path
+import subprocess
+import tarfile
+
+# location of images on disc
+disc_root='/scratch/hadpb/logbook_images/retrieved_SA'
+
+with open('to_retrieve.txt','r') as f:
+    archived = [line.rstrip() for line in f.readlines()]
+
+for tf in archived:
+
+    # directory to retrieve to
+    rdir = "%s/%s" % (disc_root,os.path.dirname(tf[48:]))
+    if not os.path.isdir(rdir):
+        os.makedirs(rdir)
+    if os.path.exists("%s/%s" % (rdir,os.path.basename(tf))):
+        continue
+
+    # Retrieve ob file from MASS
+    proc = subprocess.Popen("moo get %s %s" % 
+                                              (tf,rdir),
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               shell=True)
+    (out, err) = proc.communicate()
+    if len(err)!=0:
+        print(err)
+        raise Exception("Failed to retrieve %s" % tf)
+ 
